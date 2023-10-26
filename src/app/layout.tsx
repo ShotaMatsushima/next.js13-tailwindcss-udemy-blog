0a90f4821a55b0d71395674fbe2049445105ef5f
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-10-26 09:05:30 +0900

    Create header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from './Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>
-        <header>Header</header>
-        {children}</body>
+      <body className='container mx-auto bg-slate-700'>
+        <Header/>
+        {children}
         <footer>Footer</footer>
+      </body>
     </html>
   )
 }
