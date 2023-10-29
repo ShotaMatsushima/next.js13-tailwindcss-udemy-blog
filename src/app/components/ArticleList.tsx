53926a1666913cb8581309c3d354d77d30c3fcc4
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-10-29 12:48:56 +0900

    Add article style

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className="shadow my-4">
+        <Link href="#" className='hover:opacity-75'>
           <Image
             src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
             alt=""
@@ -14,14 +14,67 @@ const ArticleList = () => {
             height={300}
           />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="">Next.jsの勉強中</Link>
-          <p>By shotacode Published on 2023/10/29</p>
-          <Link href="#">
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pd-4 font-bold'>Technology</Link>
+          <Link
+            href="#"
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/10/29</p>
+          <Link href="#" className='text-slate-900 pb-6'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quisquam tempore animi pariatur repellendus ex suscipit beatae fuga? Voluptate repellat dolore eveniet rerum, aut aspernatur officia ut repellendus voluptatibus eaque.
           </Link>
-          <Link href="#">続きを読む</Link>
+          <Link href="#" className='text-pink-800 hover:text-black'>続きを読む</Link>
+        </div>
+      </article>
+      <article className="shadow my-4">
+        <Link href="#" className='hover:opacity-75'>
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=3"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pd-4 font-bold'>Technology</Link>
+          <Link
+            href="#"
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/10/29</p>
+          <Link href="#" className='text-slate-900 pb-6'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quisquam tempore animi pariatur repellendus ex suscipit beatae fuga? Voluptate repellat dolore eveniet rerum, aut aspernatur officia ut repellendus voluptatibus eaque.
+          </Link>
+          <Link href="#" className='text-pink-800 hover:text-black'>続きを読む</Link>
+        </div>
+      </article>
+      <article className="shadow my-4">
+        <Link href="#" className='hover:opacity-75'>
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=4"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pd-4 font-bold'>Technology</Link>
+          <Link
+            href="#"
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/10/29</p>
+          <Link href="#" className='text-slate-900 pb-6'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quisquam tempore animi pariatur repellendus ex suscipit beatae fuga? Voluptate repellat dolore eveniet rerum, aut aspernatur officia ut repellendus voluptatibus eaque.
+          </Link>
+          <Link href="#" className='text-pink-800 hover:text-black'>続きを読む</Link>
         </div>
       </article>
     </div>
