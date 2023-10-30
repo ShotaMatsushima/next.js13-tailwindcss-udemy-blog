2e7369865134427b1263047c6ad1a0e54da5e6a1
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-10-30 20:14:10 +0900

    Add blog edit page

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,43 +1,55 @@
-import { Article } from '@/types'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { Article } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 type ArticleListProps = {
-  articles: Article[]
-}
+  articles: Article[];
+};
 
-const ArticleList = ({ articles }: ArticleListProps)  => {
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
         <article className="shadow my-4" key={article.id}>
-          <Link href={`articles/${article.id}`} className='hover:opacity-75'>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt=""
               width={1280}
               height={300}
             />
           </Link>
-          <div className='bg-white flex flex-col justify-start p-6'>
-            <Link href="#" className='text-blue-700 pd-4 font-bold'>Technology</Link>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link href="#" className="text-blue-700 pd-4 font-bold">
+              Technology
+            </Link>
             <Link
               href={`articles/${article.id}`}
-              className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
             >
               {article.title}
             </Link>
-            <p className='text-sm pb-3 text-slate-900'>Published on {article.createdAt}</p>
-            <Link href={`articles/${article.id}`} className='text-slate-900 pb-6'>
+            <p className="text-sm pb-3 text-slate-900">
+              Published on {article.createdAt}
+            </p>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 pb-6"
+            >
               {article.content}
             </Link>
-            <Link href={`articles/${article.id}`} className='text-pink-800 hover:text-black'>続きを読む</Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-pink-800 hover:text-black"
+            >
+              続きを読む
+            </Link>
           </div>
         </article>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ArticleList
+export default ArticleList;
