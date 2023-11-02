6e216ff0e3c16d1f573f44a5cbdc748bbbb1b71c
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-11-02 09:27:59 +0900

    Add post blog function

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,33 +1,66 @@
-  import React from 'react'
+"use client";
 
-  const CreateBlogPage = () => {
-    return (
-      <div className='min-h-screen py-8 px-4 md:px-12'>
-        <h2 className='text-2xl font-bold md-4'>ブログ新規作成</h2>
+import { createArticle } from "@/blog.api";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useState } from "react";
 
-        <form className='bg-slate-200 p-6 rounded shadow-lg'>
-          <div className='mb-4'>
-            <label className='text-gray-700 text-sm font-bold md-2'>URL</label>
-            <input
-              type="text"
-              className='shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none'
-            />
-          </div>
-          <div className='mb-4'>
-            <label className='text-gray-700 text-sm font-bold md-2'>タイトル</label>
-            <input
-              type="text"
-              className='shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none'
-            />
-          </div>
-          <div className='mb-4'>
-            <label className='text-gray-700 text-sm font-bold md-2'>本文</label>
-            <textarea className='shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none'/>
-          </div>
-          <button type="submit" className='py-2 px-4 border rounded-md bg-orange-300'>作成</button>
-        </form>
-      </div>
-    )
-  }
+const CreateBlogPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
 
-  export default CreateBlogPage;
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await createArticle(id, title, content);
+
+    router.push("/");
+    router.refresh();
+  };
+  return (
+    <div className="min-h-screen py-8 px-4 md:px-12">
+      <h2 className="text-2xl font-bold md-4">ブログ新規作成</h2>
+
+      <form
+        className="bg-slate-200 p-6 rounded shadow-lg"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label className="text-gray-700 text-sm font-bold md-2">URL</label>
+          <input
+            type="text"
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setId(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-700 text-sm font-bold md-2">
+            タイトル
+          </label>
+          <input
+            type="text"
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-700 text-sm font-bold md-2">本文</label>
+          <textarea
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="py-2 px-4 border rounded-md bg-orange-300"
+        >
+          作成
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateBlogPage;
